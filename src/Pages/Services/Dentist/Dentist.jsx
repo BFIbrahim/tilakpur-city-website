d1fb2df86bcfd808ebc4482f9fa1b338c88dc223
Md Ibrahim <mdibrahim36194@gmail.com>
2025-01-07 22:40:06 +0600

    Dentist Data updated

--- src/Pages/Services/Dentist/Dentist.jsx
+++ src/Pages/Services/Dentist/Dentist.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import SingleDentist from './SingleDentist';
+import MissingAlert from '../../../Components/MissingAlert/MissingAlert';
 
 const Dentist = () => {
 
@@ -7,19 +8,22 @@ const Dentist = () => {
 
     useEffect(() => {
         fetch('dentist.json')
-        .then(res => res.json())
-        .then(data => setDentist(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setDentist(data))
+    }, [])
 
     return (
-        <div className='md:grid grid-cols-2 gap-5'>
-            {
-                dentist.map(dent => <SingleDentist 
-                    key={dent.id}
-                    dentist={dent}
-                ></SingleDentist>)
-            }
-        </div>
+        <>
+            <div className='md:grid grid-cols-2 gap-5'>
+                {
+                    dentist.map(dent => <SingleDentist
+                        key={dent.id}
+                        dentist={dent}
+                    ></SingleDentist>)
+                }
+            </div>
+            <MissingAlert></MissingAlert>
+        </>
     );
 };
 
